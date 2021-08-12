9c8c9adb9dd4ae982aae786bf6041bc0ec3258cb
Jklein019 <88823413+Jklein019@users.noreply.github.com>
2021-08-12 12:02:43 +0300

    Add files via upload
    The same method with main function.

---
+++ TimeEncryptMain.java
@@ -0,0 +1,38 @@
+import java.time.*;
+import java.util.Scanner;
+/**
+ * This class encrypting text using non fixed offset to encrypt messages.
+ * Using ASCII table to dechipher every character in the massage.
+ * The encryption depeneds on the time of the action, using it as start time.
+ * @author Jony Klein
+ * @version 11.08.2021
+ */
+public class TimeEncryptMain
+{
+    public static void main (String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        
+        long et = System.currentTimeMillis(); //encryption time.
+        int et1 = (int)(et % 255), te = 4, ev = et1+ 2; //+2 to avoid ceasar breaking.
+        //et1 is the offset, evaluated from et. te is the time encryption value, ev is encryption value.
+        String msg = "", encryptionTime = String.valueOf(et), encrypted = "";
+        
+        System.out.println("please enter massage to encrypt:");
+        msg = scan.nextLine();
+        
+        for(int i = 0; i < msg.length(); i++)
+        {
+            encrypted += (char)(msg.charAt(i)+ ev);
+            ev += 2;
+        }
+        
+        for(int j = 0; j < encryptionTime.length(); j++)
+        {
+            encrypted += (char)(encryptionTime.charAt(j)+ te);
+            te += 2;
+        }
+        
+        System.out.println(encrypted);
+    }
+}
